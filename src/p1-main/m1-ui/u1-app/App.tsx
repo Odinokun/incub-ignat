40bcc1cb3425e9977130f021f01eae186484b1df
Odinokun <odinokun@gmail.com>
2022-07-18 17:50:07 +0200

    did lesson #2

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -3,13 +3,14 @@ import s from './App.module.css'
 import React from 'react'
 
 import HW1 from '../../../p2-homeworks/h1/HW1'
+import HW2 from '../../../p2-homeworks/h2/HW2';
 
 function App() {
   return (
     <div className={s.App}>
       <h1 className='title'>react homeworks:</h1>
       <HW1/>
-      {/*<HW2/>*/}
+      <HW2/>
       {/*<HW3/>*/}
       {/*<HW4/>*/}
       {/*<HW5/>*/}
