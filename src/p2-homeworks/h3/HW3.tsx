4e24daeedc5013474abea2c5a3197e6313260848
Odinokun <odinokun@gmail.com>
2022-08-13 11:19:21 +0200

    commit

--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -1,27 +1,26 @@
 import React, {useState} from 'react'
 import GreetingContainer from './GreetingContainer'
 
-// types
 export type UserType = {
-    _id: string // need to fix any
-    name: string // need to fix any
+  _id: string
+  name: string
 }
 
 // уровень работы с глобальными данными
 function HW3() {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+  const [users, setUsers] = useState<Array<UserType>>([])
 
-    const addUserCallback = (name: string) => { // need to fix any
-        setUsers([]) // need to fix
-    }
+  const addUserCallback = (name: string) => {
+    setUsers([]) // need to fix
+  }
 
-    return (
-      <div className="container">
-        <h2 className='subtitle'>homework #3</h2>
-            {/*should work (должно работать)*/}
-            <GreetingContainer users={users} addUserCallback={addUserCallback}/>
-        </div>
-    )
+  return (
+    <div className="container">
+      <h2 className='subtitle'>homework #3</h2>
+      {/*should work (должно работать)*/}
+      <GreetingContainer users={users} addUserCallback={addUserCallback}/>
+    </div>
+  )
 }
 
 export default HW3
